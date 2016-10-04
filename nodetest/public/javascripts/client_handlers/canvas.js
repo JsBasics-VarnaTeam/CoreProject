de73e61e561214c40acd42c0a1af0e76dd172071
Sideroff <ivan.sideroff@gmail.com>
2016-10-04 12:04:48 +0300

    fixed: objects now move like tanks

--- nodetest/public/javascripts/client_handlers/canvas.js
+++ nodetest/public/javascripts/client_handlers/canvas.js
@@ -1,9 +1,10 @@
-//left = 37
-// up = 38
-// right = 39
-// down = 40
+
 var canvas = new fabric.StaticCanvas('canvas', { width: 1200, height: 600 });
 canvas.backgroundColor = 'rgb(100,100,200)'
+//radians are needed for movement
+fabric.Object.prototype.getAngleInRadians = function() {
+    return this.getAngle() / 180 * Math.PI;
+};
 
 // create a rectangle object
 var rect = new fabric.Rect({
@@ -11,7 +12,9 @@ var rect = new fabric.Rect({
     top: 100,
     fill: 'red',
     width: 20,
-    height: 20
+    height: 20,
+    originX: 'center',
+    originY: 'center',
 });
 
 // "add" rectangle onto canvas
--- nodetest/public/javascripts/client_handlers/onKeydown.js
+++ nodetest/public/javascripts/client_handlers/onKeydown.js
@@ -1,31 +1,37 @@
 
 window.addEventListener('keydown',this.check,false);
 function check(e) {
+    let angleOffset = 0;
     let leftOffset = 0;
     let topOffset = 0;
+    //pixel per step
+    let speed = 1;
     var code = e.keyCode;
+
     //if arrow key pressed prevent default (scroll)
     if(code==37 || code == 38 || code == 39 || code == 40){
         e.preventDefault();
     }
-    switch (code) {
-        //Left key
-        case 37:
-            leftOffset-=10;
-            break;
-        //Up key
-        case 38:
-            topOffset-=10;
-            break;
-        //Right key
-        case 39:
-            leftOffset+=10;
-            break;
-        //Down key
-        case 40:
-            topOffset+=10;
-            break;
+    //Up key
+    if(code==38) {
+        topOffset -= speed * Math.cos(rect.getAngleInRadians());
+        leftOffset += speed * Math.sin(rect.getAngleInRadians());
     }
-    rect.set({'left': rect.getLeft() + leftOffset, 'top': rect.getTop() + topOffset});
+    //Down key
+    if(code==40){
+        topOffset += speed * Math.cos(rect.getAngleInRadians());
+        leftOffset -= speed * Math.sin(rect.getAngleInRadians());
+    }
+    //Left key
+    if(code==37) {
+        angleOffset -= 1;
+    }
+    //Right key
+    if(code==39){
+        angleOffset += 1
+    }
+
+    rect.set({'left': rect.getLeft() + leftOffset, 'top': rect.getTop() + topOffset, 'angle': rect.getAngle() + angleOffset});
+    console.log(rect.getAngle());
     canvas.renderAll();
 }
