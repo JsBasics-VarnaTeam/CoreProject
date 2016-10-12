b4c3eac79f52a400f2c2cbaa43d1ec28edc874ff
kolev9605 <kolev9605@gmail.com>
2016-10-12 13:22:34 +0300

    event handlers for client prediction updates made async

--- public/javascripts/client_handlers/game_handlers/on_new_bullet.js
+++ public/javascripts/client_handlers/game_handlers/on_new_bullet.js
@@ -6,28 +6,30 @@ client.on('new-bullet', (data) => {
         return
     }
 
-    let offset = new Date().getTime() - (data.time  + avglat / 2 + serverTimeOffset)
+    setTimeout(() => {
+        let offset = new Date().getTime() - (data.time  + avglat / 2 + serverTimeOffset)
 
-    let moves = offset / 30
-    moves = moves < 1 ? 1 : moves
-    // console.log('moves: ' + moves)
-    let speed = moves * 5
+        let moves = offset / 30
+        moves = moves < 1 ? 1 : moves
+        // console.log('moves: ' + moves)
+        let speed = moves * 5
 
-    data.bullet.x -= data.bullet.xOffset * speed
-    data.bullet.y -=  data.bullet.yOffset * speed
+        data.bullet.x -= data.bullet.xOffset * speed
+        data.bullet.y -=  data.bullet.yOffset * speed
 
-    let bullet = {x: data.bullet.x, y: data.bullet.y, xOffset: data.bullet.xOffset, yOffset: data.bullet.yOffset}
-    let bulletData = {
-        radius: 4,
-        fill: 'black',
-        left: bullet.x,
-        top: bullet.y,
-        originX: 'center',
-        originY: 'center'
-    }
+        let bullet = {x: data.bullet.x, y: data.bullet.y, xOffset: data.bullet.xOffset, yOffset: data.bullet.yOffset}
+        let bulletData = {
+            radius: 4,
+            fill: 'black',
+            left: bullet.x,
+            top: bullet.y,
+            originX: 'center',
+            originY: 'center'
+        }
 
-    bullet.gameObj = new fabric.Circle(bulletData)
+        bullet.gameObj = new fabric.Circle(bulletData)
 
-    players[data.id].bullets.push(bullet)
-    canvas.add(bullet.gameObj)
+        players[data.id].bullets.push(bullet)
+        canvas.add(bullet.gameObj)
+    }, 0, data)
 })
